e7b168a749df9337adffdfa2b63095c9adf458b7
SeanRose1701 <57074761+SeanRose1701@users.noreply.github.com>
2022-04-24 16:33:14 +0100

    renamed layout

--- src/main/java/com/example/demo2/Application.java
+++ src/main/java/com/example/demo2/Application.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("layout.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Hello!");
         stage.setResizable(false);
